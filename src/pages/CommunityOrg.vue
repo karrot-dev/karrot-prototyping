3646502e792ee229798a012a29aa93578e921b43
brnsolikyl <bruno.solikyl.se>
2021-01-11 22:15:14 +0100

    tabs

--- src/pages/CommunityOrg.vue
+++ src/pages/CommunityOrg.vue
@@ -1,27 +1,48 @@
 <template>
-    <div class="q-pa-lg">
-      <div class="text-h6">Vision</div>
-      <q-field stack-label>
-        <template v-slot:control>
-          <div class="self-center full-width no-outline" tabindex="0">Whatever you wrote will be displayed here.</div>
-        </template>
-      </q-field>
-      <div class="text-h6">Decision-making</div>
-      <q-field stack-label>
-        <template v-slot:control>
-          <div class="self-center full-width no-outline" tabindex="0">Whatever you wrote will be displayed here.</div>
-        </template>
-      </q-field>
-      <div class="text-h6">Agreements</div>
-      <q-field stack-label>
-        <template v-slot:control>
-          <div class="self-center full-width no-outline" tabindex="0">Whatever you wrote will be displayed here.</div>
-        </template>
-      </q-field>
+  <div class="q-pa-md">
+    <div class="q-gutter-y-md" style="max-width: 600px">
+      <q-page class="flex-center">
+        <q-tabs
+           v-model="tab"
+          active-color="primary"
+          indicator-color="primary"
+          align="justify"
+          narrow-indicator
+        >
+          <q-tab name="agreements" icon="fas fa-handshake" label="Agreements" />
+          <q-tab name="vision" icon="emoji_objects" label="Vision" />
+          <q-tab name="decision-making" icon="thumbs_up_down" label="Decision-making" />
+        </q-tabs>
+
+        <q-separator />
+
+        <q-tab-panels v-model="tab" animated>
+          <q-tab-panel name="agreements">
+            <div class="text-h4">Main Agreement</div>
+            Whatever you wrote in the previous section will be displayed here
+            <div class="text-h5">Other agreements</div>
+            Here you can add other agreements
+          </q-tab-panel>
+          <q-tab-panel name="vision">
+            <div class="text-h4">Vision</div>
+            Whatever you wrote in the previous section will be displayed here
+          </q-tab-panel>
+          <q-tab-panel name="decision-making">
+            <div class="text-h4">Decision-making</div>
+            Whatever you wrote in the previous section will be displayed here
+          </q-tab-panel>
+        </q-tab-panels>
+      </q-page>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      tab: 'agreements'
+    }
+  }
 }
 </script>
