7a41a2f4aeab04a9768d6e0cea5c3865419d2249
brnsolikyl <bruno.solikyl.se>
2021-02-02 16:02:18 +0100

    add agreement details communityView

---
+++ src/pages/AgreementDetails.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="q-pa-md">
+    <div class="q-gutter-y-md" style="max-width: 600px">
+      <q-page class="flex-center">
+        <div class="text-h4">Agreement Details</div>
+          <q-card
+            class="q-pa-md"
+            v-for="agreement in group.agreements"
+            :key="agreement.title"
+          >
+            <q-badge
+              floating color="primary"
+              align="right"
+              label="Approved dd-mm-yyyy"
+            />
+            <div class="text-h5">{{ agreement.title }}</div>
+            <q-card-section>
+              <q-field
+                class="q-mb-md"
+                label="Summary"
+                filled
+                stack-label
+              >
+                <template v-slot:control>
+                  <div class="self-center full-width no-outline">
+                    {{ agreement.summary }}
+                  </div>
+                </template>
+              </q-field>
+
+              <q-field
+                label="Content"
+                filled
+                stack-label
+              >
+                <template v-slot:control>
+                  <div class="self-center full-width no-outline">
+                    <p>Values</p>
+                    <p>Respect</p>
+                    <p>Respect means to us acceptance, openness and fairness.</p>
+                    <p>Reliability</p>
+                    <p>Reliability stems from our own intrinsic motivation.</p>
+                    <p>Sense of responsibility</p>
+                    <p>A sense of responsibility implies for us sustainability, transparency and nonmonetary dealing with rescued foods. In the sense of this responsibility towards rescued foods, we consider it more important to quickly process rescued foods than giving them away in a charitable way.
+                    <p>Community</p>
+                    <p>The structure and the procedure of our meetings, coupled with individual responsibility, aims to prevent conflicts. If possible, feedback is given using non-violent communication. In the event of conflicts, the Well-being Manager is there to mediate.</p>
+                  </div>
+                </template>
+              </q-field>
+
+            <div class="q-mt-sm">
+                <q-chip
+                  dense
+                  color="secondary"
+                >
+                Sharing is Caring
+                </q-chip>
+                <q-chip
+                  dense
+                  color="secondary"
+                >
+                Sustainability
+                </q-chip>
+              </div>
+            </q-card-section>
+
+            <q-card-section class="q-pa-sm">
+              <q-btn class="q-mr-md" label="Show History" color="primary" />
+              <q-btn label="Propose change" color="primary" />
+            </q-card-section>
+          </q-card>
+      </q-page>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      group: this.$root.$data.group
+    }
+  }
+}
+</script>
---
+++ src/pages/CommunityView.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="q-pa-md">
+    <div class="q-gutter-y-md" style="max-width: 600px">
+      <q-page class="flex-center">
+        <q-tabs
+           v-model="tab"
+          active-color="primary"
+          indicator-color="primary"
+          align="justify"
+          narrow-indicator
+        >
+          <q-tab name="agreements" icon="fas fa-handshake" label="Agreements" />
+          <q-tab name="vision" icon="emoji_objects" label="Vision" />
+          <q-tab name="decision-making" icon="thumbs_up_down" label="Decision-making" />
+        </q-tabs>
+
+        <q-separator />
+
+        <q-tab-panels v-model="tab" animated>
+          <q-tab-panel name="agreements">
+            <q-card
+              class="q-pa-md"
+              v-for="agreement in group.agreements"
+              :key="agreement.title"
+            >
+              <q-badge floating color="primary" align="right" label="Pending" />
+              <div class="text-h5">Title of agreement</div>
+              <q-card-section>
+                <q-field label="Summary" filled stack-label>
+                  <template v-slot:control>
+                    <div class="self-center full-width no-outline">
+                      {{ agreement.summary }}
+                    </div>
+                  </template>
+                </q-field>
+                <div class="q-mt-sm">
+                  <q-chip dense color="secondary">Sharing is Caring</q-chip>
+                  <q-chip dense color="secondary">Sustainability</q-chip>
+                </div>
+              </q-card-section>
+              <q-card-section class="q-pa-sm">
+                <q-btn label="Show details" color="primary" to=agreement-details />
+              </q-card-section>
+            </q-card>
+            <template>
+              <q-btn class="q-mt-lg" round color="primary" icon="fa fa-plus" />
+            </template>
+          </q-tab-panel>
+
+          <q-tab-panel name="vision">
+            <q-field filled hint="Last updated 15-06-2019">
+              <template v-slot:control>
+                <div class="self-center full-width no-outline">
+                  {{ group.vision }}
+                </div>
+              </template>
+            </q-field>
+             <div class="q-mt-md">
+                <q-btn
+                  to="/vertical"
+                  fab-mini color="primary"
+                  label="Edit Vision"
+                  icon="fas fa-pencil-alt"
+                />
+             </div>
+          </q-tab-panel>
+
+          <q-tab-panel name="decision-making">
+            <q-field filled>
+              <template v-slot:control>
+                <div class="self-center full-width no-outline">
+                  {{ group.governanceDescription }}
+                </div>
+              </template>
+            </q-field>
+          </q-tab-panel>
+        </q-tab-panels>
+      </q-page>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      tab: 'agreements',
+      group: this.$root.$data.group
+    }
+  }
+}
+</script>
