318bc8c73c1ce9d6809edfc4d2191caf313f52f4
Nick Sellen <git@nicksellen.co.uk>
2021-06-08 10:38:17 +0100

    Make it more clear you can start discussion first

--- src/pages/AgreementProposalEdit.vue
+++ src/pages/AgreementProposalEdit.vue
@@ -72,112 +72,150 @@
           <q-input
             v-model="agreement.reason"
             label="Reason for proposal (optional)"
-            hint="Why are making this proposal? Is it a response to something that happened? What are hoping it will achieve?"
+            hint="Why are making this proposal? Is it a response to something that happened? What are you hoping it will achieve?"
             outlined
             autogrow
             input-style="min-height: 65px;"
           />
         </q-card-section>
 
-        <q-card-section>
-          <div class="text-h6">Proposal</div>
+        <q-card-section v-if="isNew && isChangeToExistingAgreement">
+          <q-list>
+            <q-item tag="label" v-ripple>
+              <q-item-section avatar>
+                <q-radio v-model="changes" val="later" color="teal" />
+              </q-item-section>
+              <q-item-section>
+                <q-item-label>Start the discussion first</q-item-label>
+                <q-item-label caption>You can propose changes later</q-item-label>
+              </q-item-section>
+            </q-item>
+
+            <q-item tag="label" v-ripple>
+              <q-item-section avatar>
+                <q-radio v-model="changes" val="now" color="teal" />
+              </q-item-section>
+              <q-item-section>
+                <q-item-label>Propose changes first</q-item-label>
+                <q-item-label caption>You can discuss and refine them later</q-item-label>
+              </q-item-section>
+            </q-item>
+          </q-list>
+
         </q-card-section>
 
-        <q-card-section>
-          <q-input
-            v-model="agreement.title"
-            label="Agreement Title"
-            outlined
-          />
-        </q-card-section>
+        <template v-if="changes === 'now' || !isChangeToExistingAgreement">
+          <q-card-section v-if="isNew && isChangeToExistingAgreement && changes === 'now'">
+            <div class="text-h6 q-mb-lg">Proposed changes</div>
+            <q-banner class="bg-orange-1 q-pa-md q-mt-md">
+              <template #avatar>
+                <q-icon name="fas fa-edit" color="primary" />
+              </template>
+              Propose the changes you'd like to discuss, you can still make changes later
+            </q-banner>
+          </q-card-section>
 
-        <!--
-        <q-card-section v-if="selectedValues.length === 0">
-          <q-banner class="bg-orange-1 q-pa-md q-mb-md">
-            Add the values you are trying to encourage to help people to understand why the agreement is important.
-          </q-banner>
-          <q-btn
-            label="Add values"
-            unelevated
-            rounded
-            color="green"
-            @click="editValues = true"
-          />
-        </q-card-section>
-        <q-card-section v-else>
-          <values :values="agreement.values" class="q-mb-lg q-pa-md bg-grey-2"/>
-          <q-btn
-            label="Edit values"
-            unelevated
-            rounded
-            color="green"
-            @click="editValues = true"
-          />
-        </q-card-section>
+          <q-card-section v-if="isNew && !isChangeToExistingAgreement">
+            <div class="text-h6">New Agreement</div>
+          </q-card-section>
 
-        <custom-dialog v-model="editValues">
-          <template #message>
-            <div
-              v-for="category in valueCategoryDefinitions"
-              :key="category.name"
-            >
-              <div class="text-h4 q-my-md q-ml-md">
-                <small style="white-space: nowrap">To help support</small>&nbsp;
-                <strong>{{ category.name }}</strong>&nbsp;
-                <small style="white-space: nowrap">we encourage:</small>
-              </div>
-              <q-btn
-                v-for="value in valuesForCategory(category.name)"
-                :key="value.name"
-                :label="value.name"
-                unelevated
-                rounded
-                @click="toggleValue(value)"
-                class="q-mr-sm q-mb-sm"
-                :class="selectedValues.includes(value) ? 'bg-green text-white' : ''"
-              />
-            </div>
-          </template>
-          <template #actions>
-            <QBtn
-              v-close-popup
-              flat
-              color="primary"
-              label="Done"
+          <q-card-section>
+            <q-input
+              v-model="agreement.title"
+              label="Agreement Title"
+              outlined
             />
-          </template>
+          </q-card-section>
 
-        </custom-dialog>
-        -->
+          <!--
+          <q-card-section v-if="selectedValues.length === 0">
+            <q-banner class="bg-orange-1 q-pa-md q-mb-md">
+              Add the values you are trying to encourage to help people to understand why the agreement is important.
+            </q-banner>
+            <q-btn
+              label="Add values"
+              unelevated
+              rounded
+              color="green"
+              @click="editValues = true"
+            />
+          </q-card-section>
+          <q-card-section v-else>
+            <values :values="agreement.values" class="q-mb-lg q-pa-md bg-grey-2"/>
+            <q-btn
+              label="Edit values"
+              unelevated
+              rounded
+              color="green"
+              @click="editValues = true"
+            />
+          </q-card-section>
 
-        <q-card-section>
-          <q-input
-            v-model="agreement.summary"
-            label="Summary (optional)"
-            outlined
-            autogrow
-            input-style="min-height: 65px; max-height: 600px;"
-          />
-        </q-card-section>
+          <custom-dialog v-model="editValues">
+            <template #message>
+              <div
+                v-for="category in valueCategoryDefinitions"
+                :key="category.name"
+              >
+                <div class="text-h4 q-my-md q-ml-md">
+                  <small style="white-space: nowrap">To help support</small>&nbsp;
+                  <strong>{{ category.name }}</strong>&nbsp;
+                  <small style="white-space: nowrap">we encourage:</small>
+                </div>
+                <q-btn
+                  v-for="value in valuesForCategory(category.name)"
+                  :key="value.name"
+                  :label="value.name"
+                  unelevated
+                  rounded
+                  @click="toggleValue(value)"
+                  class="q-mr-sm q-mb-sm"
+                  :class="selectedValues.includes(value) ? 'bg-green text-white' : ''"
+                />
+              </div>
+            </template>
+            <template #actions>
+              <QBtn
+                v-close-popup
+                flat
+                color="primary"
+                label="Done"
+              />
+            </template>
 
-        <q-card-section>
-          <q-input
-            v-model="agreement.content"
-            label="Agreement text"
-            outlined
-            autogrow
-            input-style="min-height: 200px; max-height: 600px;"
-          />
-        </q-card-section>
+          </custom-dialog>
+          -->
 
-        <q-card-section class="q-pa-sm">
-          <q-btn
-            class="q-mr-md"
-            :label="isNew ? 'Create' : 'Save'"
-            color="primary"
-            @click="save()"
-          />
-        </q-card-section>
+          <q-card-section>
+            <q-input
+              v-model="agreement.summary"
+              label="Summary (optional)"
+              outlined
+              autogrow
+              input-style="min-height: 65px; max-height: 600px;"
+            />
+          </q-card-section>
+
+          <q-card-section>
+            <q-input
+              v-model="agreement.content"
+              label="Agreement text"
+              outlined
+              autogrow
+              input-style="min-height: 200px; max-height: 600px;"
+            />
+          </q-card-section>
+
+          <q-card-section class="q-pa-sm">
+            <q-btn
+              class="q-mr-md"
+              :label="isNew ? 'Create proposal' : 'Save'"
+              color="primary"
+              @click="save()"
+            />
+          </q-card-section>
+
+        </template>
       </q-card>
     </div>
   </q-page>
@@ -228,7 +266,12 @@ export default {
       agreement,
       dateMask: 'YYYY-MM-DD',
       editValues: false,
-      valueCategoryDefinitions
+      valueCategoryDefinitions,
+      changeOptions: [
+        { label: 'Start the discussion first', value: 'bat' },
+        { label: 'Suggest changes, then start the discussion', value: 'friend', color: 'green' }
+      ],
+      changes: 'later'
     }
   },
   methods: {
